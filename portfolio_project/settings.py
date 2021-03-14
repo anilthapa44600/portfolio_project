c8bd4120605afae5d9ba9f1d616e3082d06d9e0c
anilthapa44600 <anilthapa44600@gmail.com>
2021-03-14 15:40:57 +0545

    staticupdate

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_DIRS = [os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),
                'C:/Users/Anil/PycharmProjects/portfolio_project/portfolio/static']
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
