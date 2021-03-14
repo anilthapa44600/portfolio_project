3647deec7e9d8bdce7e44cc597026ee130d410fe
anilthapa44600 <anilthapa44600@gmail.com>
2021-03-14 15:38:33 +0545

    static

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles'),
-               'C:\Users\Anil\PycharmProjects\portfolio_project\portfolio\static']
+STATIC_ROOT = [os.path.join(BASE_DIR, 'static'),
+               'C:/Users/Anil/PycharmProjects/portfolio_project/portfolio/static']
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
